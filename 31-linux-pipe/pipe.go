e15f78a8fc6ba9c82c72171d84b6d2e74269bb36
Jacek Wysocki <jacek.wysocki@gmail.com>
2015-01-12 21:51:59 +0100

    better linux pipe example with stdin pipe detection

--- 31-linux-pipe/pipe.go
+++ 31-linux-pipe/pipe.go
@@ -3,8 +3,17 @@ package main
 import "os"
 import "log"
 import "io/ioutil"
+import "fmt"
 
 func main() {
-	bytes, err := ioutil.ReadAll(os.Stdin)
-	log.Println(err, string(bytes))
+
+	stat, _ := os.Stdin.Stat()
+	if (stat.Mode() & os.ModeCharDevice) == 0 {
+		fmt.Println("data is being piped to stdin")
+		bytes, err := ioutil.ReadAll(os.Stdin)
+		log.Println(err, string(bytes))
+	} else {
+		fmt.Println("stdin is from a terminal")
+	}
+
 }
