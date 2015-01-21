588aa2437a6695600063ce3b6f23f4e3a4bb7f46
Jacek Wysocki <jacek.wysocki@gmail.com>
2015-01-21 22:46:20 +0100

    removed http server call

--- 47-json-rpc/rpc.go
+++ 47-json-rpc/rpc.go
@@ -53,8 +53,6 @@ func startServer() {
 	server := rpc.NewServer()
 	server.Register(arith)
 
-	server.HandleHTTP(rpc.DefaultRPCPath, rpc.DefaultDebugPath)
-
 	l, e := net.Listen("tcp", ":8222")
 	if e != nil {
 		log.Fatal("listen error:", e)
